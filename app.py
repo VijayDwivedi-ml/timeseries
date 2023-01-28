2ba90bdab5a084d5f428f14e4cca3e1649ec3a6f
VijayDwivedi-ml <93728191+VijayDwivedi-ml@users.noreply.github.com>
2023-01-28 20:48:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,68 @@
 import streamlit as st
-from PIL import Image
-import requests
 import pandas as pd
 import numpy as np
 import sqlite3
+#from PIL import Image
+import requests
+#from MyFunction import * 
 
-# Weather data for any city
+# Web page basic configuration
+st.set_page_config(
+    page_title="Asthma Management App",
+    page_icon="⚕️",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-def find_current_weather(city):
-    API_KEY = "642b399f532d6a45a46c3a8c83d0a18e"
-    base_url  = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    weather_data = requests.get(base_url).json()
-#   st.json(weather_data)
-    lat = weather_data['coord']['lat']
-    lon = weather_data['coord']['lon']
-    base_url2 = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_KEY}"
-    weather_data2 = requests.get(base_url2).json()
-#    st.json(weather_data2)
+# # Author details for contacting him
+# col1,col2 = st.columns(2)
+# with col1:
+    # st.sidebar.image("IMG/VIJAY_DWIVEDI.jpg", width = 80)
+# with col1:
+    # st.sidebar.markdown('<a href="mailto:vijay.dwivedi@ge.com"> 📧 Vijay Dwivedi</a>', unsafe_allow_html=True)
+
+
+# Main page basic details
+
+st.title("Asthma Management App 📱️")
+st.sidebar.title('Please Provide Details')
+
+select1 = st.sidebar.selectbox(' 🧒 👨 👴', ['19-30','31-40', '41-50', 'Above 50'])
+
+select2 = st.sidebar.selectbox('🧑‍🤝‍🧑', ['Male','Female'])
+
+select3 = st.sidebar.selectbox('🚬, 🍷', ['Yes','No', 'Either'])
+
+select4 = st.sidebar.selectbox('🍲, 💊', ['Yes','No'])
+
+select5 = st.sidebar.selectbox('Asthma medicine missed', ['Yes','No'])
+
+select5 = st.sidebar.selectbox('When you have taken last medicine', ['Yesterday','One week back', 'Two Week Back'])
+ 
+# Main function for the app
+ 
+def main():
+    st.sidebar.header("The Weather ☁️")
+    city = st.sidebar.text_input("Enter the City", 'Bengaluru' ).lower()
+    if st.sidebar.button("Find"):
+        general,temperature, humidity, icon, AQI = find_current_weather(city)
+        col_1,col_2, col_3 = st.columns(3)
+        with col_1:
+            st.metric(label = "Temperature 🌡️",value=f"{temperature}°C", delta_color="normal")
+        with col_2:
+            st.metric(label = "Humidity 🚿",value=f"{humidity}%")
+        with col_3:
+            st.write('Weather ☁️: ', general)
+            st.image(icon)
+        # with col_4:
+            # st.metric(label = "AQI 😷 ",value=f"{AQI}")
+        # df2 = DataBase()[DataBase()['Age'] == select1]
+        # df3 = df2[df2['Gender'] == select2]
+        # df4 = df3[df3['SmokingHabit'] == select3]
+        # st.write(df4.head())
+        st.markdown("<h2 style='text-align: center; color: lightgreen;'>Thank You for Using the App</h2>", unsafe_allow_html=True)
+#        st.balloons()
+
     
-    try:
-        general = weather_data['weather'][0]['main']
-        icon_id = weather_data['weather'][0]['icon']
-        temperature = round(weather_data['main']['temp'])
-        humidity = round(weather_data['main']['humidity'])
-        icon = f"http://openweathermap.org/img/wn/{icon_id}@2x.png"
-     #   lat = weather_data['coord']['lat']
-     #   lon = weather_data['coord']['lon']
-        aqi = weather_data2['list'][0]['main']['aqi']
-        AQI = 0
-        if aqi == 1:
-            AQI = 'Good'
-        elif aqi == 2:
-            AQI = 'Fair'
-        elif aqi == 3:
-            AQI = 'Moderate'
-        elif aqi == 4:
-            AQI = 'Poor'
-        elif aqi == 5:
-            AQI = 'Very Poor'
-            
-        
-    except KeyError:
-        st.error("City Not Found")
-        st.stop()
-    return general,temperature, humidity, icon, AQI
-    
-#Create a SQL connection to our SQLite database
-
-@st.cache(persist = True)
-def DataBase():
-    con = sqlite3.connect("asthmadb.db")
-    df = pd.read_sql_query('select * from Asthma_Data_File', con)
-    con.close()
-    return df
+if __name__ == '__main__':
+    main()
