f0c8485e43610d94fa3ae981ed93903623f7d84c
VijayDwivedi-ml <93728191+VijayDwivedi-ml@users.noreply.github.com>
2023-01-28 19:36:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,66 @@
+#import warnings
+#warnings.filterwarnings('ignore')  # Hide warnings
+import datetime as dt
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sqlite3
+import matplotlib.pyplot as plt
+import seaborn as sns
 from PIL import Image
+import os
+from ta.volatility import BollingerBands
+from ta.trend import MACD
+from ta.momentum import RSIIndicator
 
-#Create a SQL connection to our SQLite database
-con = sqlite3.connect("chemistry.db")
-df = pd.read_sql_query('SELECT * from chemistry', con)
-con.close()
+##################
+# Set up sidebar #
+##################
 
-st.title('Product Identification based on Reactant based of Chemical Reaction')
-st.sidebar.title('Chemistry Project by Vedang Dwivedi, XII')
-#image = Image.open('https://github.com/VijayDwivedi-ml/timeseries/blob/main/vedang_pic_final.JPG')
-#st.sidebar.image('image', width = 100)
+# Add in location to select image.
 
-select1 = st.sidebar.selectbox('Reactant Selection', ['Alkyne','Alkene','Alkanes','Alcohol','Alcohol(Secondary)','Aldehyde','Ketone','Carboxylic Acid','Ether','Amine', ' '])
-st.write('You Selected Reactant:', select1)
-st.write('\n')
-st.write('\n')
-st.write('\n')
-df2 = df[df['Reactant1'] == select1]
-st.write(df2)
+option = st.sidebar.selectbox('symbol', ('ONMOBILE', 'GATI'))
 
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
+st.write((option))
 
 
-select2 = st.sidebar.selectbox('Process-Reagent Selection', ['Hydrogenation','Halogenation','Ozonolysis','Diboration','Dedydration','Oxidation','Grignard Reagent','Tollens Reagent','Fehlings', 'Reagent', ' '])
-st.write('You Selected Reactant and Process-Reagent:', select1, ',',select2)
-st.write('\n')
-st.write('\n')
-st.write('\n')
-df3 = df2[df2['Process_Reagent'] == select2]
-st.write(df3)
-
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
-
-select3 = st.sidebar.selectbox('Product Selection', ['Alkene', 'Halo Alkane', 'Aldehyde', 'Alcohol', 'Alkane',' '])
-st.write('You Selected Reactant, Process-Reagent and Product:', select1, ',', select2, ', ', select3)
-st.write('\n')
-st.write('\n')
-st.write('\n')
-df4 = df3[df3['Product1'] == select3]
-st.write(df4)
-
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
-st.write('\n')
+##############
+# Stock data #
+##############
 
 
-st.write("##################################################################")
+df = pd.read_csv('data/ONMOBILE.csv')
+
+df.reset_index(inplace=True)
+df.set_index("Date", inplace=True)
+
+indicator_bb = BollingerBands(df['Close'])
+
+bb = df
+bb['bb_h'] = indicator_bb.bollinger_hband()
+bb['bb_l'] = indicator_bb.bollinger_lband()
+bb = bb[['Close','bb_h','bb_l']]
+
+macd = MACD(df['Close']).macd()
+
+rsi = RSIIndicator(df['Close']).rsi()
 
 
+###################
+# Set up main app #
+###################
+n=100
+st.write('Stock Bollinger Bands')
+
+st.line_chart(bb.tail(n))
+
+progress_bar = st.progress(0)
+
+st.write('Stock Moving Average Convergence Divergence (MACD)')
+st.area_chart(macd.tail(n))
+
+st.write('Stock RSI ')
+st.line_chart(rsi.tail(n))
 
 
-
+st.write('Recent data ')
+st.dataframe(df.tail(n))
